72e5cde478e36c89f456f382d541b08e85d571a0
Michael VanMeter <MGVdec2000@gmail.com>
2022-03-08 20:52:19 -0500

    Add the Web API Project
    Adds a blank Web API project and tweaks it

---
+++ TRMDataManager/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+
+namespace TRMDataManager.Controllers
+{
+    [Authorize]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            return new string[] { "value1", "value2", userId };
+        }
+
+        // The following version of the same method above allows you more flexibility in the http response, but requires additioal documentation and makes for more difficult reading.
+
+        // GET api/values
+        //public IHttpActionResult Get()
+        //{
+        //    string userId = RequestContext.Principal.Identity.GetUserId();
+        //    return Ok(new string[] { "value1", "value2", userId });
+        //}
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
