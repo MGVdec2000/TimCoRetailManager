e728f2ca289ef53e8a9cf21d1ad10057c63cca18
Michael VanMeter <MGVdec2000@gmail.com>
2022-03-12 10:40:11 -0500

    Wire Up Login for to API
    Connects login form button to Authenticate method of the API (/Token).
    
    Gets back the authentication token or throws exception if failed.

---
+++ TRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,72 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRMDesktopUI.Helpers;
+
+namespace TRMDesktopUI.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+        private string _userName;
+
+        private IAPIHelper _apiHelper;
+
+        public string UserName
+        {
+            get { return _userName; }
+            set
+            {
+                _userName = value;
+                NotifyOfPropertyChange(() => UserName);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        private string _password;
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => _password);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        public bool CanLogIn
+        {
+            get
+            {
+                bool output = false;
+                if (UserName?.Length > 0 && Password?.Length > 0)
+                {
+                    output = true;
+                }
+                return output;
+            }
+        }
+
+        public LoginViewModel(IAPIHelper apiHelper)
+        {
+            _apiHelper = apiHelper;
+        }
+
+        public async Task LogIn()
+        {
+            try
+            {
+                var result = await _apiHelper.Authenticate(UserName, Password);
+            }
+            catch (Exception Ex)
+            {
+                
+            }
+        }
+
+    }
+}
